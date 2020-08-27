23ee499ed8cd9cb96f7b1111e6bce854d50eb0f4
oakleykt <oakley@keltecweapons.com>
2020-08-27 07:40:25 -0400

    update to code and notebook

--- Assignment 01/Assignment 1.py
+++ Assignment 01/Assignment 1.py
@@ -37,7 +37,7 @@ def calculateDB(e1, n1, e2, n2):
 
     if eastings > 0 and northings > 0:
         quadrant = 'First Quadrant E+, N+'
-        b = math.degrees(math.atan((n2-n1)/(e2-e1)))
+        b = math.degrees(math.atan((e2-e1)/(n2-n1)))
     elif eastings > 0 and northings < 0:
         quadrant = 'Second Quadrant E+, N-'
         b = 180+math.degrees(math.atan((e2-e1)/(n2-n1)))
@@ -50,28 +50,55 @@ def calculateDB(e1, n1, e2, n2):
 
     return d, b, eastings, northings, quadrant
 '''
-Call all coordinates and print them out with a traveled distance.
+Either calls all coordinates listed in the assignment with a traveled distance or
+allows the user to input two sets of coordinates. Then prints the output to the terminal.
 '''
-coordlist = [
-    [511005, 3208379, 509938, 3208346, 'Browns Bay Canoe Launch to Sign 3/8'],
-    [509938, 3208346, 509948, 3208025, 'Sign 3/8 to Sign 4/7'],
-    [509948, 3208025, 510284, 3208171, 'Sign 4/7 to Sign 5/6'],
-    [510284, 3208171, 510397, 3208018, 'Sign 5/6 to Sign 6/5'],
-    [510397, 3208018, 511005, 3208379, 'Sign 6/5 to Browns Bay Canoe Launch'],
-    ]
-totaldist = []
-print() # Blank line to Start
-print('#'*50)
-for item in coordlist:
-    
-    d, b, eastings, northings, quadrant = calculateDB(item[0], item[1], item[2], item[3])
-    print(item[4])
-    print(quadrant)
-    print(f'Eastings {eastings}')
-    print(f'Northings {northings}')
-    print(f'Distance: {d:.3f} kilometers\nBearing: {b:.3f}\xb0 Degrees')
-    print()
-    totaldist.append(d)  
-     
-print(f'Total Traveled Distance is {sum(totaldist):.3f} kilometers')
-print('#'*50)
+
+while True:
+    question = input('Enter Eastings and Northings (M)anually or use (A)ssignment Coordinates (M or A)?').upper()
+
+    if question == 'A':
+        coordlist = [
+        [511005, 3208379, 509938, 3208346, 'Browns Bay Canoe Launch to Sign 3/8'],
+        [509938, 3208346, 509948, 3208025, 'Sign 3/8 to Sign 4/7'],
+        [509948, 3208025, 510284, 3208171, 'Sign 4/7 to Sign 5/6'],
+        [510284, 3208171, 510397, 3208018, 'Sign 5/6 to Sign 6/5'],
+        [510397, 3208018, 511005, 3208379, 'Sign 6/5 to Browns Bay Canoe Launch'],
+        ]
+
+        totaldist = []
+        print() # Blank line to Start
+        print('#'*50)
+        for item in coordlist:
+            
+            d, b, eastings, northings, quadrant = calculateDB(item[0], item[1], item[2], item[3])
+            print(item[4])
+            print(quadrant)
+            print(f'Eastings {eastings}')
+            print(f'Northings {northings}')
+            print(f'Distance: {d:.3f} kilometers\nBearing: {b:.3f}\xb0 Degrees')
+            print()
+            totaldist.append(d)  
+            
+        print(f'Total Traveled Distance is {sum(totaldist):.3f} kilometers')
+        print('#'*50)
+        break
+    elif question == 'M':
+        e1 = int(input('Enter Eastings Starting Point: '))
+        n1 = int(input('Enter Northings Starting Point: '))
+        e2 = int(input('Enter Eastings Ending Point: '))
+        n2 = int(input('Enter Northings Ending Point: '))
+
+        d, b, eastings, northings, quadrant = calculateDB(e1, n1, e2, n2)
+        
+        print('#'*50)
+        print(quadrant)
+        print(f'Eastings {eastings}')
+        print(f'Northings {northings}')
+        print(f'Distance: {d:.3f} kilometers\nBearing: {b:.3f}\xb0 Degrees')
+        print()
+        print('#'*50)
+        break
+    else:
+        print('Enter a valid option.')
+
