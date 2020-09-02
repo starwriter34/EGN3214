4d558ae8455fc0b23047aa3249964a7c2bfa941e
Chris Oakley <modelpainter34@gmail.com>
2020-09-01 21:23:44 -0400

    started creating assignment 2

--- Assignment 02/Assignment 2.py
+++ Assignment 02/Assignment 2.py
@@ -3,4 +3,22 @@ Chris Oakley
 Interpolation
 08/31/2020
 EGN3214 - Assignment 2
+
+https://youtu.be/Sp4EuKFYMHc?t=598
 '''
+
+import numpy as np
+
+# xp = [0.35, 1.27, 3.47, 5.87]
+# fp = [9.57, 11.35, 15.65, 19.57]
+
+# xp = [1.2, 2.3, 3.4, 4.0, 5.6, 7.8, 9.3]
+# fp = [0.999, 0.99, 0.9, 0.5, 0.1, 0.01, 0.001]
+
+# print(f'Rain call chance is {np. interp(7.0, xp, fp)*100:.2f}% ')
+
+#xp = [0, 1.2, 2.5, 3.7]
+#fp = [0, 7.5, 11.4, 14.7]
+
+#print(np. interp(2.0, xp, fp))
+#print(np. interp(0.5, xp, fp))
