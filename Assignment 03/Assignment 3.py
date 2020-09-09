e00619a520808685e57ca0faa85533782189413c
Chris Oakley <modelpainter34@gmail.com>
2020-09-09 06:18:55 -0400

    Assignment 3 Created

---
+++ Assignment 03/Assignment 3.py
@@ -0,0 +1,18 @@
+'''
+Chris Oakley
+Present Value
+08/31/2020
+EGN3214 - Assignment 3
+
+Variables:
+
+rate - Intreset Rate per period
+nper - NUmber of compounding periods
+pmt - Payment
+fv - Future value
+
+
+
+'''
+
+import numpy as np
