7d6d7793041078737e7a696edf2bba0594746144
Chris Oakley <modelpainter34@gmail.com>
2020-09-09 20:01:49 -0400

    Assignment 3 code is finished

--- Assignment 03/Assignment 3.py
+++ Assignment 03/Assignment 3.py
@@ -6,13 +6,74 @@ EGN3214 - Assignment 3
 
 Variables:
 
+cash - What you have
 rate - Intreset Rate per period
-nper - NUmber of compounding periods
-pmt - Payment
+nper - Number of compounding periods
+pmt - Payment(Revenue Generated)
 fv - Future value
 
 
-
+np.pv() - Present Value
+np.fv() - Future Value
 '''
 
-import numpy as np
+import numpy_financial as np
+
+cash = [10000, 9000, 5000, 3000, 0]
+pmt = [500, 600, 1000, 1200, 1500]
+rate = 0.05
+nper = 10
+continue_yn='y'
+question = 'ask'
+
+def investmentGB(pv, fv):
+    if pv > fv:
+        investment = "Bad Investment"
+    
+    elif pv < fv:
+        investment = "Good Investment"
+    
+    print('*'*50)
+    print()
+    print(f'The Future Value: {fv:.2f}')
+    print(f'The Present Value: {pv:.2f}')
+    print()
+    print(investment)
+    print()
+    print('*'*50)
+
+    
+
+while continue_yn=='y':
+
+    if question == 'ask':
+        question = input('Do you wish to (M)anually Calculate your PV & FV, or run the (A)ssignment Numbers (M or A)?').upper()
+    else:
+        question = 'M'
+
+    if question == 'M':
+        print()
+        cash = float(input('Enter in the Original Price: '))
+        rate = float(input("Enter in the Intreset Rate (i.e. 5% is 0.05): "))
+        nper = float(input("Enter in the Number of Years: "))
+        pmt = float(input("Enter in the Yearly Revenue: "))
+
+        pv = np.pv(rate, nper,0,-cash)
+        fv = np.fv(rate, nper,-pmt,pv)
+
+        investmentGB(pv, fv)
+        
+        print()
+        continue_yn = input('Do you wish to continue? (Y or N)').lower()
+        print()
+
+    elif question == 'A':
+        
+        for (i, j) in zip(cash, pmt):
+            
+            pv = np.pv(rate, nper,0,-i)
+            fv = np.fv(rate, nper,-j ,pv)
+
+            investmentGB(pv, fv)
+        break
+
