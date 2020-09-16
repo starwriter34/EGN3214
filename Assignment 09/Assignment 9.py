4804c221442fa0421389987fea3c3a0975e8628c
oakleykt <oakley@keltecweapons.com>
2020-09-16 13:55:38 -0400

    Update to Assignment 9

--- Assignment 09/Assignment 9.py
+++ Assignment 09/Assignment 9.py
@@ -1,12 +1,22 @@
 import math
+import time
 numbreak = 1000
+start = time.time()
+break_for = False
 
 for a in range(1,numbreak):
-    for b in range(a,numbreak):
-        for c in range(b,numbreak):
+    for b in range(a+1,numbreak):
+        for c in range(b+1,numbreak):
             if (a+b+c)==numbreak and (a**2+b**2)==c**2:
                 print(a*b*c)
-                break
+                print(f'Time Taken {time.time()-start} in seconds!')
+                break_for = True
+            
+        if break_for == True:
+            break
+    if break_for == True:
+        break
+
 # 31875000
 # n = 100
 
