a8e5ece3e89c4f38d9f787a499d526308cec0d33
Chris Oakley <modelpainter34@gmail.com>
2020-10-05 17:52:28 -0400

    Commented Assignment 9

--- Assignment 09/Assignment 9.py
+++ Assignment 09/Assignment 9.py
@@ -1,7 +1,22 @@
+
+'''
+Chris Oakley
+Functions Part 1
+09/13/2020
+EGN3214 - Assignment 4
+
+Variables:
+
+a,b,c - The sides of the triangle
+numbreak - The max number the your looking for squared
+
+The end result should equal out to 31875000 as the number we are looking for.
+
+'''
 import math
-import time
+
 numbreak = 1000
-start = time.time()
+
 break_for = False
 
 for a in range(1,numbreak):
@@ -9,19 +24,8 @@ for a in range(1,numbreak):
         for c in range(b+1,numbreak):
             if (a+b+c)==numbreak and (a**2+b**2)==c**2:
                 print(a*b*c)
-                print(f'Time Taken {time.time()-start} in seconds!')
                 break_for = True
-            
         if break_for == True:
             break
     if break_for == True:
         break
-
-# 31875000
-# n = 100
-
-# for b in range(n):
-#     for a in range(1, b):
-#         c = math.sqrt( a * a + b * b)
-#         if c % 1 == 0:
-#             print(a, b, int(c))
